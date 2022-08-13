c3e9ed9909ef68887e6675dc4a01375d40d7e7f7
tpaphysics <physics.thiago@gmail.com>
2022-08-12 21:04:46 -0300

    feat: configuração do clientKafka no main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { Logger as logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { randomUUID } from 'crypto';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'user',
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: `user-consumer-${randomUUID()}`,
+          allowAutoTopicCreation: true,
+        },
+      },
+    },
+  );
+  logger.log('[*] Started user-engine');
+  await app.listen();
 }
 bootstrap();
