aee5e630b6bb4d7e537374dd1359e08044a8ca88
tpaphysics <physics.thiago@gmail.com>
2022-08-13 13:04:56 -0300

    chore: inserção de tipagens em users.service e users.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,33 +3,35 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Observable } from 'rxjs';
+import { User } from '@prisma/client';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @MessagePattern('users.create')
-  create(@Payload() createUserDto: CreateUserDto) {
+  create(@Payload() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @MessagePattern('users.findAll')
-  findAll() {
+  findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @MessagePattern('users.findOne')
-  findOne(@Payload() id: string) {
+  findOne(@Payload() id: string): Promise<User> {
     return this.usersService.findOne(id);
   }
 
   @MessagePattern('users.update')
-  update(@Payload() updateUserDto: UpdateUserDto) {
+  update(@Payload() updateUserDto: UpdateUserDto): Promise<User> {
     return this.usersService.update(updateUserDto.id, updateUserDto);
   }
 
   @MessagePattern('users.delete')
-  remove(@Payload() id: string) {
+  remove(@Payload() id: string): Promise<User> {
     return this.usersService.remove(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 import { CreateUserDto } from './dto/create-user.dto';
@@ -7,17 +8,17 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto): Promise<User> {
     return await this.prisma.user.create({
       data: createUserDto,
     });
   }
 
-  async findAll() {
+  async findAll(): Promise<User[]> {
     return this.prisma.user.findMany();
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<User> {
     return await this.prisma.user.findUnique({
       where: {
         id,
@@ -25,7 +26,7 @@ export class UsersService {
     });
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto) {
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
     return this.prisma.user.update({
       where: {
         id,
@@ -34,7 +35,7 @@ export class UsersService {
     });
   }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<User> {
     return this.prisma.user.delete({
       where: {
         id,
